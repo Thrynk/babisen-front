ef141939fc6162fa8d899fdfe0295c3ac4bc6ce7
Thrynk <thocarin@gmail.com>
2020-03-04 17:34:27 +0100

    notifs

--- src/components/pages/AdminNotification.js
+++ src/components/pages/AdminNotification.js
@@ -15,25 +15,56 @@ export default class AdminNotification extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    componentDidMount() {
+        //fetch notifs
+        fetch(process.env.REACT_APP_API_URL + '/api/notifications', {method: 'GET'})
+            .then(response => response.json())
+            .then(notifs => {
+                this.setState({ listNotifications: notifs });
+            })
+            .catch(e => console.log(e));
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        //fetch notifs
+        fetch(process.env.REACT_APP_API_URL + '/api/notifications', {method: 'GET'})
+            .then(response => response.json())
+            .then(notifs => {
+                this.setState({ listNotifications: notifs });
+            })
+            .catch(e => console.log(e));
+    }
+
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value});
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        let listNotifications = Array.from(this.state.listNotifications);
+        /*let listNotifications = Array.from(this.state.listNotifications);
         listNotifications.push(this.state.notif);
-        this.setState({ listNotifications: listNotifications });
+        this.setState({ listNotifications: listNotifications });*/
 
-
+        fetch(process.env.REACT_APP_API_URL + '/api/notifications/new',
+            {method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({notification: this.state.notif, date: new Date()})})
+            .then(response => {
+                if(response.status === 200){
+                    this.forceUpdate();
+                }
+            })
+            .catch(e => console.log(e));
     }
 
     render(){
         return (
-            <div align="center">
+            <div align="center" style={{marginTop: 50}}>
             <form onSubmit={this.handleSubmit}>
                 <FormControl>
-                    <Input name="notif" value={this.state.value} onChange={this.handleChange} inputRef={this.textInput} color='Secondary'></Input>
+                    <Input name="notif" value={this.state.value} onChange={this.handleChange} inputRef={this.textInput} color='Secondary' />
                     <Button variant="contained" 
                             color="primary"
                             type="submit" 
@@ -48,7 +79,7 @@ export default class AdminNotification extends React.Component {
                 {this.state.listNotifications.map((notif, index) => {
                     return(
                         <ListItem key={index}>
-                            <ListItemText>{notif}</ListItemText>
+                            <ListItemText>{notif.notification}</ListItemText>
                         </ListItem>
                     );
                 })}
--- src/components/partials/Tournaments/ListOfAttendees.js
+++ src/components/partials/Tournaments/ListOfAttendees.js
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
+//import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+//import Avatar from '@material-ui/core/Avatar';
+//import ImageIcon from '@material-ui/icons/Image';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
 
